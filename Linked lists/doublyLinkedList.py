6d1be96d0cdc0b68d68b34cfcefb90ba90979aeb
Adjéri SABI <kod047@gmail.com>
2020-12-11 00:56:44 +0900

    Doubly linked list

--- Linked lists/doublyLinkedList.py
+++ Linked lists/doublyLinkedList.py
@@ -2,8 +2,9 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+        self.prev = None
 
-class LinkedList:
+class DoublyLinkedList:
     def __init__(self, value):
         self.head = Node(value)
         self.tail = self.head
@@ -11,6 +12,7 @@ class LinkedList:
 
     def append(self, value):
         newNode = Node(value)
+        newNode.prev = self.tail
         self.tail.next = newNode
         self.tail = newNode
         self.length += 1
@@ -19,6 +21,7 @@ class LinkedList:
     def prepend(self, value):
         newNode = Node(value)
         newNode.next = self.head
+        self.head.prev = newNode
         self.head = newNode
         self.length += 1
         return self
@@ -44,7 +47,9 @@ class LinkedList:
                     newNode = Node(value)
                     nextNode = current.next
                     current.next = newNode
+                    newNode.prev = current
                     newNode.next = nextNode
+                    nextNode.prev = newNode
                     self.length +=1
                     break
                 else:
@@ -65,6 +70,7 @@ class LinkedList:
         if index == 0: #remove head
             temp = self.head
             self.head = self.head.next
+            self.head.prev = None
             del temp
             self.length -=1
 
@@ -77,11 +83,13 @@ class LinkedList:
             self.length -=1
 
         else:
-            previousNode = self.getNodeAtIndex(index - 1)
-            toDelete = previousNode.next
+            toDelete = self.getNodeAtIndex(index)
+            previousNode = toDelete.prev
             nextNode = toDelete.next
             toDelete.next = None
+            toDelete.prev = None
             previousNode.next = nextNode
+            nextNode.prev = previousNode
             del toDelete
             self.length -= 1
 
@@ -101,14 +109,14 @@ class LinkedList:
 
 if __name__ == '__main__':
 
-    myLinkedlist = LinkedList(10)
+    myLinkedlist = DoublyLinkedList(10)
     myLinkedlist.append(15)
     myLinkedlist.append(115)
     myLinkedlist.prepend(41)
-    myLinkedlist.insert(2,225)
     myLinkedlist.insert(2,45)
     myLinkedlist.insert(0,147)
     myLinkedlist.insert(50, 89)
     print(myLinkedlist.printList())
-    myLinkedlist.remove(14)
-    # print(myLinkedlist.printList())
+    myLinkedlist.remove(0)
+    print(myLinkedlist.printList())
+    print(myLinkedlist.head.prev)
